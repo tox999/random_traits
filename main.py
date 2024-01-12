6c4e245838617e8b4dac6e7f1943b28532928ae7
tox999 <12495737+tox999@users.noreply.github.com>
2024-01-12 12:01:07 +0000

    space

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from random import Random
 import traits
 import os
 
+
 class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -14,64 +15,81 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def traits_string(generator, trait_list, number_of_traits=3, color=Colors.FAIL, sep=", "):
-  trait_list_copy = trait_list.copy()
-  selected_traits = set()
-  for _ in range(0, number_of_traits):
-    trait = generator.choice(trait_list_copy)
-    selected_traits.add(trait)
-    trait_list_copy.remove(trait)
 
-  result = ", ".join(selected_traits)
-  return f"{color}{result}{Colors.ENDC}"
+def traits_string(generator,
+                  trait_list,
+                  number_of_traits=3,
+                  color=Colors.FAIL,
+                  sep=", "):
+    trait_list_copy = trait_list.copy()
+    selected_traits = set()
+    for _ in range(0, number_of_traits):
+        trait = generator.choice(trait_list_copy)
+        selected_traits.add(trait)
+        trait_list_copy.remove(trait)
+
+    result = ", ".join(selected_traits)
+    return f"{color}{result}{Colors.ENDC}"
+
 
 def trait_generator(generator, seed, number_of_traits):
-  iteration = 0
-  while True:
-    print("\n")
-    print("Negative traits:")
-    print(traits_string(generator,traits.negative, number_of_traits, Colors.FAIL))
-    print("Neutral traits:")
-    print(traits_string(generator,traits.neutral, number_of_traits, Colors.OKBLUE))
-    print("Positive traits:")
-    print(traits_string(generator,traits.positive, number_of_traits, Colors.OKGREEN))    
-    print("\n")
-    iteration += 1
-    print("Generate another? (y/n)")    
-    answer = input("Answer: ")
-    if answer.lower() in ["y", "yes", "ye", "", " ", None]:
-      cls()
-      print(f"Seed: {seed} \nIteration: {iteration}")
-    else:
-      break
+    iteration = 0
+    while True:
+        print("\n")
+        print("Negative traits:")
+        print(
+            traits_string(generator, traits.negative, number_of_traits,
+                          Colors.FAIL))
+        print("Neutral traits:")
+        print(
+            traits_string(generator, traits.neutral, number_of_traits,
+                          Colors.OKBLUE))
+        print("Positive traits:")
+        print(
+            traits_string(generator, traits.positive, number_of_traits,
+                          Colors.OKGREEN))
+        print("\n")
+        iteration += 1
+        print("Generate another? (y/n)")
+        answer = input("Answer: ")
+        if answer.lower() in ["y", "yes", "ye", "", " ", None]:
+            cls()
+            print(f"Seed: {seed} \nIteration: {iteration}")
+        else:
+            break
+
 
 def main(*args, **kwargs):
-  cls()
-  while True:
-    print("Welcome to random traits generator.")
-    print("Pick used for generation of random traits or press enter for generate it too.")
-    seed = input("Seed: ")
-    if seed == "":
-      generator = Random()
-    else:
-      generator = Random(seed)
-    
-    print("Choose number (int) of traits to generate, 3 is used if left blank.")
-    number_of_traits = input("Number: ")
-    try:
-      number_of_traits = int(number_of_traits)
-    except:
-      number_of_traits = 3
-    
-    trait_generator(generator, seed, number_of_traits)
+    cls()
+    while True:
+        print("Welcome to random traits generator.")
+        print(
+            "Pick used for generation of random traits or press enter for generate it too."
+        )
+        seed = input("Seed: ")
+        if seed == "":
+            generator = Random()
+        else:
+            generator = Random(seed)
 
+        print(
+            "Choose number (int) of traits to generate, 3 is used if left blank."
+        )
+        number_of_traits = input("Number: ")
+        try:
+            number_of_traits = int(number_of_traits)
+        except:
+            number_of_traits = 3
+
+        trait_generator(generator, seed, number_of_traits)
 
 
 if __name__ == "__main__":
-  try:
-    main()
-  except KeyboardInterrupt:
-    print("Goodbye!")
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Goodbye!")
